77dc07cbeb907fa9559a7f32294e1eb433e8d322
Harrison Coulter <harrison@Harrisons-MacBook-Pro.local>
2018-10-10 15:57:29 +0100

    finished product. Needs refractor... badly..

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,3 +1,111 @@
 def basket_calc(books_array)
-    8.00
+
+  BookList.new(books_array).find_cheapest_combo.inject(0) {|total,set|
+     total += discount(set)
+     total
+   }.round(2)
+
+end
+
+class BookList
+
+  attr_accessor :books, :tally
+
+  def initialize(books)
+    @books = books
+  end
+
+  def make_tally
+    tally = {
+      1 => 0,
+      2 => 0,
+      3 => 0,
+      4 => 0,
+      5 => 0
+    }
+    tally = @books.inject(tally) do |tally,book_id|
+        tally[book_id] += 1
+        tally
+    end
+  end
+
+  def find_cheapest_combo
+
+      new_sets = seperate_books.select do |array|
+        array.length != 3 && array.length != 5
+      end
+
+      sets_with_3 = seperate_books.select do |array|
+        array.length == 3
+      end
+
+      sets_with_5 = seperate_books.select do |array|
+        array.length == 5
+      end
+
+      if sets_with_3.size == 0 || sets_with_5.size == 0
+        return seperate_books
+      end
+
+      #test atm = [1,2,3,4,5] [1,2,3]
+
+      array_to_use     = sets_with_3.size < sets_with_5.size ? sets_with_3 : sets_with_5
+      array_not_to_use = sets_with_3.size < sets_with_5.size ? sets_with_5 : sets_with_3
+
+      array_to_use.each_with_index do |current_array, index|
+        (1..5).each do |i|
+          if !(current_array.include? i) && current_array.size == 3
+            array_not_to_use[index].delete(i)
+            current_array << i
+          elsif current_array.size == 5
+            current_array.delete(i)
+            array_not_to_use[index] << i
+          end
+        end
+      end
+
+      #sets of 4
+      array = array_to_use.concat(array_not_to_use).concat(new_sets)
+  end
+
+  def seperate_books
+
+    array_of_arrays = []
+
+    make_tally.each do |key,value|
+      i = 0
+      value.times {
+
+        if array_of_arrays[i].nil?
+            array_of_arrays[i] = []
+        end
+
+        array_of_arrays[i] << key
+        i+=1
+      }
+    end
+
+    array_of_arrays
+  end
+
+end
+
+def discount(books)
+
+  standard_price = books.size * 8.00
+
+  if books.uniq.size == 1
+    return standard_price
+  end
+
+  case books.size
+  when 2
+    standard_price * 0.95
+  when 3
+    standard_price * 0.90
+  when 4
+    standard_price * 0.80
+  else
+    standard_price * 0.75
+  end
 end
--- spec/harry_potter_spec.rb
+++ spec/harry_potter_spec.rb
@@ -7,9 +7,51 @@ RSpec.describe "" do
   end
 
   it "show that 2 of the same book costs 16.00 euro" do
+    expect(basket_calc([1,1])).to eq(16.00)
   end
 
   it "show that 2 different books costs 15.20 euro" do
+    expect(basket_calc([1,2])).to eq(15.20)
+  end
+
+  it "show that 3 different books costs 21.60 euro" do
+    expect(basket_calc([1,2,3])).to eq(21.60)
+  end
+
+  it "show that 4 different books costs 32.00 euro" do
+    expect(basket_calc([1,2,3,4])).to eq(25.60)
+  end
+
+  it "show that 5 different books costs 30.00 euro" do
+    expect(basket_calc([1,2,3,4,5])).to eq(30.00)
+  end
+
+  it "show that books of the same type will not be counted in the discount" do
+    expect(basket_calc([1,2,1])).to eq(23.20)
+  end
+
+  it "show that 2 sets of books that should be discounted will discount both sets" do
+    expect(basket_calc([1,2,3,1,2,3])).to eq(43.20)
+  end
+
+  it "show that sets of books that should be discounted will discount all sets" do
+    expect(basket_calc([1,2,3,1,2,3,1])).to eq(51.20)
+  end
+
+  it "show that sets of books will always find the cheapest discount" do
+    expect(basket_calc([1,1,2,2,3,3,4,5])).to eq(51.20)
+  end
+
+  it "show that sets of books will always find the cheapest discount" do
+    expect(basket_calc([1,1,2,2,3,3,4,5,1,1,2,2,3,3,4,5])).to eq(102.40)
+  end
+
+  it "show that sets of books will always find the cheapest discount" do
+    expect(basket_calc([1,1,1,2,2,2,3,3,3,4,5])).to eq(72.80)
+  end
+
+  it "show that sets of books will always find the cheapest discount" do
+    expect(basket_calc([1,1,1,1,1,1,1,2,2,2,2,2,2,3,3,3,3,3,3,4,4,4,5,5,5])).to eq(161.60)
   end
 
 end
